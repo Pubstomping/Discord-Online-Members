4851ecb8323b845ecdab04f0da28d4ed6d3c34eb
Pubstomped <biorunswz@outlook.com>
2026-09-19 13:48:16 +0100

    Create data.py

---
+++ data.py
@@ -0,0 +1,31 @@
+GAMES_LIST = [
+    # Call of Duty
+    "Call of Duty: Modern Warfare III", "Call of Duty: Black Ops 6", "Call of Duty: Warzone",
+    "Call of Duty: WWII", "Call of Duty: Black Ops II", "Call of Duty 4: Modern Warfare",
+    
+    # Rainbow Six & Fortnite
+    "Rainbow Six Siege", "Rainbow Six Extraction", "Fortnite", "Fortnite Festival", "LEGO Fortnite",
+    
+    # Apex & Horizon
+    "Apex Legends", "Horizon Zero Dawn", "Horizon Forbidden West",
+    
+    # Need for Speed
+    "Need for Speed Most Wanted", "Need for Speed Underground 2", "Need for Speed Heat", "Need for Speed Unbound",
+    
+    # GTA V
+    "Grand Theft Auto V"
+]
+
+MUSIC_LIST = [
+    # Lil Peep & Lil Tecca
+    "Lil Peep - Star Shopping", "Lil Peep - Benz Truck", "Lil Peep - Witchblades",
+    "Lil Tecca - Ransom", "Lil Tecca - 500lbs", "Lil Tecca - Did It Again",
+    # Lil Pump & MGK
+    "Lil Pump - Boss", "Lil Pump - Esskeetit", "MGK - bloody valentine", "MGK - emo girl",
+    # Juice WRLD & Eminem
+    "Juice WRLD - Lucid Dreams", "Juice WRLD - Robbery", "Eminem - Lose Yourself", "Eminem - Houdini",
+    # Pop Smoke, Lil Durk & King Von
+    "Pop Smoke - Dior", "Pop Smoke - For the Night", "Lil Durk - All My Life", "King Von - Crazy Story",
+    # UK Drill, Grime & Electronic
+    "Central Cee - Doja", "Dave & Central Cee - Sprinter", "Skepta - Shutdown", "Fred again.. - Rumble"
+]
